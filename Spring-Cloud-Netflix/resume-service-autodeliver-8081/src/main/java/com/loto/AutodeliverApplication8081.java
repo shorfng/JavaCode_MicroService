ef1f529fc96cf615290a4bde671ccfed3896fbdc
蓝田_Loto <shorfng@126.com>
2021-11-08 22:37:43 +0800

    Eureka使用流程 - 将服务消费者注册到 Eureka 服务中心

--- Spring-Cloud-Netflix/resume-service-autodeliver-8081/src/main/java/com/loto/AutodeliverApplication8081.java
+++ Spring-Cloud-Netflix/resume-service-autodeliver-8081/src/main/java/com/loto/AutodeliverApplication8081.java
@@ -2,10 +2,12 @@ package com.loto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AutodeliverApplication8081 {
     public static void main(String[] args) {
         SpringApplication.run(AutodeliverApplication8081.class, args);
