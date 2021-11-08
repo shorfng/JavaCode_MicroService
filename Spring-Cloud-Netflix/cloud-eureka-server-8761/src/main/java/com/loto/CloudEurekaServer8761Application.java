e219de36988d309fd8a4cd550aee2bd85d8c3b9a
蓝田_Loto <shorfng@126.com>
2021-11-08 17:09:42 +0800

    搭建单例 Eureka Server 服务注册中⼼

---
+++ Spring-Cloud-Netflix/cloud-eureka-server-8761/src/main/java/com/loto/CloudEurekaServer8761Application.java
@@ -0,0 +1,13 @@
+package com.loto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer  // 声明当前项目为Eureka服务
+public class CloudEurekaServer8761Application {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaServer8761Application.class, args);
+    }
+}
